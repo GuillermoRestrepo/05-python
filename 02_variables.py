2acee754bf4710b68fef5abaf5094e31b99522ee
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-10 17:39:08 -0500

    02_variables: definir variables en python

---
+++ 02_variables.py
@@ -0,0 +1,15 @@
+"""
+En python, una variable se define:
+  · (nombre = valor).
+
+Nomenclatura:
+  · Deben iniciar con LETRA o GUION BAJO ( _ ).
+  · Se utiliza snake_case.
+  · Solo pueden contener LETRAS, NUMEROS, GUIONES BAJOS.
+"""
+
+esto_es_1_variable= "Hola"
+print(esto_es_1_variable)
+
+_esta_es_la_variable_2= "Adios"
+print(_esta_es_la_variable_2)
