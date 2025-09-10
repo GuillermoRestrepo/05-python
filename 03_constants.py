8c6eed4171a0eff44ee0534faaa845c48573dafe
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-10 17:48:43 -0500

    03_constants: definir constantes en python

---
+++ 03_constants.py
@@ -0,0 +1,16 @@
+"""
+En python, una constante se define:
+  · (NOMBRE = valor).
+
+Nomenclatura:
+  · Deben de estar escritas en MAYUSCULAS.
+  · Deben iniciar con LETRA o GUION BAJO ( _ ).
+  · Se utiliza snake_case.
+  · Solo pueden contener LETRAS, NUMEROS, GUIONES BAJOS.
+"""
+
+ESTO_ES_1_CONSTANTE= 3.1416
+print(ESTO_ES_1_CONSTANTE)
+
+_ESTA_ES_LA_CONSTANTE_2= 9.8
+print(_ESTA_ES_LA_CONSTANTE_2)
