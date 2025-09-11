721f6fd742fde02e91b1533de7d6bcc274e851c1
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-11 01:38:57 -0500

    10_assignment_operators: operadores de asignación en python.

---
+++ 10_assignment_operators.py
@@ -0,0 +1,31 @@
+"""  OPERADORES DE ASIGNACION  """
+# =
+x= 7
+
+# +=
+x+= 3
+print(f"1. {x}")
+
+# -=
+x-= 3
+print(f"2. {x}")
+
+# *=
+x*= 3
+print(f"3. {x}")
+
+# /=
+x/= 3
+print(f"4. {x}")
+
+# //=
+x//= 3
+print(f"5. {x}")
+
+# %=
+x%= 3
+print(f"6. {x}")
+
+# **=
+x**= 3
+print(f"7. {x}")
