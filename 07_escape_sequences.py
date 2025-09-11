bdb92b8c18ceca859f37f11ee4c4f91fb851ae88
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-11 00:48:37 -0500

    07_escape_sequences: representar saltos de lineas, tabulaciones, comillas y slash invertido en strings.

---
+++ 07_escape_sequences.py
@@ -0,0 +1,14 @@
+"""  SECUENCIA DE ESCAPE
+Se utiliza el slash invertido (\), para escibir caracteres especiales en los strings.
+"""
+
+# COMILLAS SIMPLES Y DOBLES
+print('\'Representar comillas simples\'')
+print("\"Representar comillas dobles\"")
+
+# SALTO DE LINEA Y TABULACIONES
+print("Aquí va -->\n un salto de línea.")
+print("Aquí va -->\t una tabulación.")
+
+# SLASH INVERTIDO
+print("Asi se representa un SLASH INVERITDO (\\).")
