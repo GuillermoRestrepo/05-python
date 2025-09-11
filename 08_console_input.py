6ffc4d8009df1ce7c18a9b82198c7588ee725d7a
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-11 01:02:41 -0500

    08_console_input: entrada de datos por consola en python.

---
+++ 08_console_input.py
@@ -0,0 +1,8 @@
+"""  ENTRADA POR CONSOLA
+Todo se ingresa en forma de texto.
+Se puede utilizar las funciones para convetir a otro tipo de datos.
+"""
+nombre= input("Ingresa tu nombre: ")
+edad= int(input("Ingresa tu edad: "))
+
+print(f"Nombre: {nombre} - Edad: {edad}")
