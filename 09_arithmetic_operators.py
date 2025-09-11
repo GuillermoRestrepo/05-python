cd83343de8de2fa16ed27fec0820d2eb42533b50
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-11 01:26:40 -0500

    09_arithmetic_operators.py: operadores aritmeticos en python.

---
+++ 09_arithmetic_operators.py
@@ -0,0 +1,24 @@
+"""  OPERADORES ARITMETICOS  """
+numero_1= 5
+numero_2= 3
+
+# SUMA (+)
+print(f"Suma: {numero_1+numero_2}")
+
+# RESTA (-)
+print(f"Resta: {numero_1-numero_2}")
+
+# MULTIPLICACION (*)
+print(f"Multiplicacion: {numero_1*numero_2}")
+
+# DIVISION (/)
+print(f"Division: {numero_1/numero_2}")
+
+# DIVISION ENTERA (//)
+print(f"Division entera: {numero_1//numero_2}")
+
+# MODULO / RESIDUO (%)
+print(f"Modulo/Residuo: {numero_1%numero_2}")
+
+# EXPONENTE (**)
+print(f"{numero_1**numero_2}")
