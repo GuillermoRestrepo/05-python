552dcff209c7f53f40f7fe1ce2857c432c0939a7
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-09-11 21:13:06 -0500

    12_comparison_operators: operadores de comparación en python.

---
+++ 12_comparison_operators.py
@@ -0,0 +1,23 @@
+"""  OPERADORES DE COMPARACION  """
+a = 5
+b = 3
+
+# IGUAL (==) (is)
+print(a == b)
+print(a is b)
+
+# DIFERENTE (!=) (is not)
+print(a != b)
+print(a is not b)
+
+# MAYOR QUE (>)
+print(a > b)
+
+# MENOR QUE (<)
+print(a < b)
+
+# MAYOR O IGUAL QUE (>=)
+print(a >= 5)
+
+# MENOR O IGUAL QUE (<=)
+print(b <= 2)
